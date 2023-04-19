89ccad94f390e2e46f93a0e5aa08b74c01754495
Juliano Silva <cont.juliano@outlook.com>
2023-04-19 08:46:35 -0300

    fix: destructuring import request and removed optional params to request

--- src/infra/adapters/CounterAPIGateway.ts
+++ src/infra/adapters/CounterAPIGateway.ts
@@ -1,4 +1,4 @@
-import request from '../../../tests/request'
+import { request } from '../../../tests/request'
 import { ValidationException } from '../../core/exceptions/ValidationException'
 import { CounterGateway } from './CounterGateway'
 
@@ -7,9 +7,7 @@ export class CounterAPIGateway implements CounterGateway {
     async hit(url: string): Promise<any> {
         const response = await request(
             `https://api.countapi.xyz/hit/${url}/visits`,
-            'GET',
-            {},
-            {}
+            'GET'
         )
         const status = response.status
         if (status !== 200) throw new ValidationException('Error to hit $url')
