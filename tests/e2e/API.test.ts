17d89df0221738106caa1a6d84000fd131e8afd1
Juliano Silva <cont.juliano@outlook.com>
2023-04-19 08:47:55 -0300

    test: mocked request and added asserts

--- tests/e2e/API.test.ts
+++ tests/e2e/API.test.ts
@@ -1,9 +1,29 @@
-import request from '../request'
+import { request } from '../request'
+
+const url = 'http://localhost:3000/visits/ton.com.br'
+
+jest.mock('../request', () => {
+    return {
+        request: jest.fn(() => {
+            const expectedStatusCode = 200
+            return Promise.resolve({
+                status: expectedStatusCode,
+                data: {
+                    url,
+                    visits: 1
+                }
+            })
+        })
+    }
+})
+
+afterAll(() => {
+    jest.restoreAllMocks()
+})
 
 test('Should /visits/:id', async () => {
-    const response = await request(
-        'http://localhost:3000/visits/ton.com.br',
-        'GET'
-    )
+    const response = await request(url, 'GET')
     expect(response.status).toBe(200)
+    expect(response.data.url).toBe(url)
+    expect(response.data.visits).toBe(1)
 })
