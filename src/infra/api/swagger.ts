406f3a25b4e07593151d8b9943baa67d7421daa1
Juliano Silva <cont.juliano@outlook.com>
2023-04-20 20:42:27 -0300

    chore: updated swagger doc

--- src/infra/api/swagger.ts
+++ src/infra/api/swagger.ts
@@ -5,7 +5,7 @@ export default {
     openapi: '3.0.0',
     info: {
         title: 'Counter API',
-        version: '1.4.1'
+        version: '1.7.0'
     },
     servers: [
         {
@@ -24,7 +24,7 @@ export default {
                         name: 'url',
                         required: true,
                         schema: {
-                            type:'string',
+                            type: 'string',
                             example: 'ton.com.br'
                         }
                     }
@@ -51,7 +51,8 @@ export default {
                         }
                     }
                 }
-            }
+            },
+
         },
         '/visits/hit/{url}': {
             post: {
@@ -64,7 +65,7 @@ export default {
                         name: 'url',
                         required: true,
                         schema: {
-                            type:'string',
+                            type: 'string',
                             example: 'ton.com.br'
                         }
                     }
@@ -75,31 +76,124 @@ export default {
                     }
                 }
             }
+        },
+        '/users': {
+            post: {
+                tags: ['Users'],
+                operationId: 'createUser',
+                description: 'Create user',
+                requestBody: {
+                    content: {
+                        'application/json': {
+                            schema: {
+                                $ref: '#/components/schemas/CreateUserInput'
+                            }
+                        }
+                    }
+                },
+                responses: {
+                    '200': {
+                        description: 'Successful response',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/CreateUserOutput'
+                                }
+                            }
+                        }
+                    },
+                    '422': {
+                        description: 'Error: Unprocessable Entity',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/UserAlreadyExists'
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+        },
+        '/users/{userId}': {
+            get: {
+                tags: ['Users'],
+                operationId: 'getUser',
+                description: 'Get user',
+                parameters: [
+                    {
+                        in: 'path',
+                        name: 'userId',
+                        required: true,
+                        schema: {
+                            type: 'string'
+                        }
+                    }
+                ],
+                responses: {
+                    '200': {
+                        description: 'Successful response',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/GetUserOutput'
+                                }
+                            }
+                        }
+                    },
+                    '422': {
+                        description: 'Error: Unprocessable Entity',
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/UserNotFound'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     },
     components: {
         schemas: {
+            UserNotFound: {
+                type: 'object',
+                properties: {
+                    code: {
+                        type:'string'
+                    },
+                    message: {
+                        type:'string',
+                        example: 'User not found'
+                    }
+                }
+            },
+            UserAlreadyExists: {
+                type: 'object',
+                properties: {
+                    code: {
+                        type: 'integer',
+                        example: 422
+                    },
+                    message: {
+                        type:'string',
+                        example: 'User Already Exists'
+                    }
+                }
+            },
             ValidationException: {
                 type: 'object',
                 properties: {
                     code: {
-                        type:'integer',
+                        type: 'integer',
                         example: 422,
                     },
                     message: {
-                        type:'string',
+                        type: 'string',
                         example: 'Error to get ton.com.br'
                     }
                 }
-            },
-            IncrementVisitsInput: {
-                type: 'object',
-                properties: {
-                    url: {
-                        type:'string'
-                    }
-                },
-                required: ['url']
             },
             GetVisitsOutput: {
                 type: 'object',
@@ -111,6 +205,53 @@ export default {
                     visits: {
                         type: 'integer',
                         example: 1
+                    }
+                }
+            },
+            CreateUserInput: {
+                type: 'object',
+                properties: {
+                    name: {
+                        type: 'string',
+                        example: 'ton'
+                    },
+                    email: {
+                        type: 'string',
+                        example: 'ton@example.com'
+                    },
+                    password: {
+                        type: 'string',
+                        example: '123456'
+                    }
+                }
+            },
+            CreateUserOutput: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'string',
+                        example: '57102d39-4d96-4385-b088-0e9e2e3c9611'
+                    }
+                }
+            },
+            GetUserOutput: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type:'string',
+                        example: '57102d39-4d96-4385-b088-0e9e2e3c9611'
+                    },
+                    name: {
+                        type:'string',
+                        example: 'ton'
+                    },
+                    email: {
+                        type:'string',
+                        example: 'ton@example.com'
+                    },
+                    isAdmin: {
+                        type: 'integer',
+                        example: 0
                     }
                 }
             }
